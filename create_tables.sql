5f74fd5b7c8a743af01bcc43dbba3b2b32eadff8
mgaleski <mpgaleski@outlook.com>
2020-04-25 14:09:27 -0500

    adding sql files

---
+++ create_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE fake_job_postings (
+     job_id int NOT NULL
+     title varchar NOT NULL
+     location varchar NOT NULL
+     company_profile varchar NOT NULL
+     required experience varchar
+     PRIMARY KEY (job_id)
+);
+
+CREATE TABLE fake_tech_postings (
+     job_id int NOT NULL
+     title varchar NOT NULL
+     location varchar NOT NULL
+     company_profile varchar NOT NULL
+     required experience varchar
+     PRIMARY KEY (job_id)
+);
+
+
